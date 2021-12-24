aa8dc0406cc9a4dddfdb929c46109352148ed45b
Ahrar Hussain <ahrarhakeem@gmail.com>
2021-12-24 12:24:50 +0530

    innerHTML, addEventListener, .value

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function savelead(){
-  console.log("Button Working.")
+
+let arr=["www.amazon.com","www.nie.ac.in","www.flipkart"]
+let input=document.getElementById("input-el")
+let ulel=document.getElementById("unordered-el")
+let saveinputbtn=document.getElementById("input-btn")
+
+  saveinputbtn.addEventListener("click",function(){
+  arr.push(input.value)
+  console.log(input.value)
+})
+
+
+// for(let i=0;i<arr.length;i++){
+//   ulel.textContent += arr[i] + " "
+// }
+//We don't want our text to be plain. We want it to be in the form of list.
+for(let i=0;i<arr.length;i++){
+  ulel.innerHTML += "<li>" +  arr[i] + "</li>"
 }
