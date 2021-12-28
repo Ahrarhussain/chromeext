f262f28064fabee1875efafbe7f28bc577f60b9c
Ahrar Hussain <ahrarhakeem@gmail.com>
2021-12-28 08:00:04 +0530

    Stitching everything together.

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 
-let arr=["www.amazon.com","www.nie.ac.in","www.flipkart"]
+let arr=[]
 let input=document.getElementById("input-el")
 let ulel=document.getElementById("unordered-el")
 let saveinputbtn=document.getElementById("input-btn")
 
-  saveinputbtn.addEventListener("click",function(){
+saveinputbtn.addEventListener("click",function(){
   arr.push(input.value)
-  console.log(arr)
+  renderleads()
 })
+function renderleads(){
+  let listitems= " "
+  for(let i=0;i<arr.length;i++){
+    listitems += "<li>" + arr[i] +  "</li>"
+    console.log(listitems)
+  }
+   ulel.innerHTML= listitems
 
-
-// for(let i=0;i<arr.length;i++){
-//   ulel.textContent += arr[i] + " "
-// }
-//We don't want our text to be plain. We want it to be in the form of list.
-let listitems= " "
-for(let i=0;i<arr.length;i++){
-  //ulel.innerHTML += "<li>" + arr[i] + "</li>"
-  //OR
-  //Using alternative way
-  //Create element
-  //set textContent
-  //append textContent to ul
-  // let z=document.createElement("li")
-  // z.textContent=arr[i]
-  // ulel.append(z)
-  // OR
-  //Improving performance
-  listitems += "<li>" + arr[i] +  "</li>"
-  console.log(listitems)
 }
- ulel.innerHTML= listitems
